ac27b15a0e8a0d6ed5ed699e7c4a3182d608e962
abhi-patil9075 <abhijit.educloud@gmail.com>
2024-08-08 15:21:58 +0530

    @Abhijit chnages
    @Abhijit chnages

--- Demo/js/index.js
+++ Demo/js/index.js
@@ -1,3 +1,5 @@
+
+
 function restrictInput(element, event) {
     
     if (event.type === "paste") {
@@ -39,9 +41,11 @@ async function submitConsentForm(){
 		let status = await validateCustomer(userInfo);
 		if(status){
 			nextStep();
+			fillUserInfoInConsentPage(userInfo);
 		}
+	}else{
+		
 	}
-	
 }
 
 function validateStringValue(string){
@@ -52,12 +56,23 @@ function validateStringValue(string){
 }
 
 function nextStep(){
+
 	$(`#wizrd_1_pro`).removeClass(`active_wizrd`).addClass('completed');
 	$(`#wizrd_2_pro`).addClass(`active_wizrd`);
 
 	$(`#step-1-div`).hide();
 	$(`#step-2-div`).show();
 	$(`.downArrow`).show();	
+}
+	
+
+function fillUserInfoInConsentPage(userInfo) {
+	let today = new Date();
+	const formattedDate = new Intl.DateTimeFormat('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).format(today);
+	$("#userName").html(userInfo.name);
+	$("#userDob").html(userInfo.dateOfBirth);
+	$("#userLan").html(userInfo.lan);
+	$("#consentDate").html(formattedDate);
 }
 
 async function validateCustomer(userInfo){
@@ -79,21 +94,49 @@ async function validateCustomer(userInfo){
 	return status;
 }
 
+async function acceptAgreement(){
 
-function fillInfoAndValidate(){
-    // get user name
+	const isChecked = $("#agreeTermAndCond").is(":checked");
+	if(isChecked){
+		// Load jsPDF library
+		const { jsPDF } = window.jspdf;
+				
+		// Create a new jsPDF instance
+		const doc = new jsPDF();
+		
+		// Capture the HTML content using html2canvas
+		const content = document.getElementById('consent-content-div-to-pdf');
+		const canvas = await html2canvas(content, { scale: 1 });
+		const imgData = canvas.toDataURL('image/png');
 
-    // get lan number
+		// Add the captured image to the PDF
+		const imgWidth = 190; // Width of the PDF page in mm
+		const pageHeight = 297; // Height of the PDF page in mm
+		const imgHeight = (canvas.height * imgWidth) / canvas.width;
+		let heightLeft = imgHeight;
+		let position = 0;
 
-    // get dob
+		doc.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight);
+		heightLeft -= pageHeight;
 
-    // send to verifiy by ajax
+		while (heightLeft >= 0) {
+			position = heightLeft - imgHeight;
+			doc.addPage();
+			doc.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight);
+			heightLeft -= pageHeight;
+		}
 
-    // get responce. if the responce is true the show the term and condition page to accept the consent  else show the wrong credintional message to user.
+		// Save the PDF
+		doc.save('hello_world.pdf');
+	}else{
+		alert(" Please check the check box to continue!")
+	}
 
+       
+
+	
 }
 
-function acceptAgriment(){
-    
-}
+
+
 
